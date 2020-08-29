53de4e73418cdc653996012b6f25e711c194023f
cdiop81 <cheikh.diop@hotmail.fr>
2020-08-29 12:55:26 +0200

    Refactoring and add start ssc

--- src/main/scala/org/formation/SparkStreamingTP3.scala
+++ src/main/scala/org/formation/SparkStreamingTP3.scala
@@ -7,11 +7,17 @@ import org.slf4j.LoggerFactory
 object SparkStreamingTP3 {
   def main(args: Array[String]): Unit = {
     val logger = LoggerFactory.getLogger(SparkStreamingTP3.getClass)
+    if (args.length < 2) {
+      logger.error("Please provide : hostname & port ")
+    }
+
     logger.info("Starting word count streaming Application")
     val conf = new SparkConf().setAppName("MySparkStreamingApp").setMaster("local[2]")
     val ssc = new StreamingContext(conf, Seconds(1))
-    val lines = ssc.socketTextStream("localhost", 9999)
+    val lines = ssc.socketTextStream(args(0), args(1).toInt)
     val wordsCounts = lines.flatMap(_.split(" ")).map(x => (x, 1)).reduceByKey(_ + _)
-    wordsCounts.print();
+    wordsCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
